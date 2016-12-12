0ed7afe1918be34299d13ef0ff919224260e522a
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2016-12-12 20:36:06 +0100

    Added simple notifications

---
+++ app/src/main/java/cat/udl/menufinder/notifications/MyFirebaseInstanceIdService.java
@@ -0,0 +1,24 @@
+package cat.udl.menufinder.notifications;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    public static final String TAG = MyFirebaseInstanceIdService.class.getName();
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token : " + refreshedToken);
+
+        sendRegistrationToServer(refreshedToken);
+
+
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+
+    }
+}
