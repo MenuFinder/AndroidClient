7e8861197afd8d44b4d8f37a3bac8c213395cdb7
MEUSBURGGER <maicolmeusburger@gmail.com>
2016-12-15 17:46:43 +0100

    Interface Principal

--- app/src/main/java/cat/udl/menufinder/database/DBManager.java
+++ app/src/main/java/cat/udl/menufinder/database/DBManager.java
@@ -1,22 +1,94 @@
 package cat.udl.menufinder.database;
 
 import java.util.List;
+import java.util.Map;
 
+import cat.udl.menufinder.models.Account;
 import cat.udl.menufinder.models.Item;
 import cat.udl.menufinder.models.ItemCategory;
 import cat.udl.menufinder.models.Menu;
+import cat.udl.menufinder.models.MenuItem;
 import cat.udl.menufinder.models.Restaurant;
+import cat.udl.menufinder.models.Review;
 
 public interface DBManager {
 
-    public void addRestaurant(Restaurant restaurant);
+    Account getValidLogin(String id, String password);
 
-    public List<Restaurant> getRestaurants();
+    List<Menu> getMenusByRestaurantId(long restaurantId);
 
-    public List<ItemCategory> getItemCategories();
+    Menu getMenuById(long menuId);
 
-    public List<Menu> getAllMenusOfRestaurant(int menu_id);
+    String addNewMenu(Menu menu);
 
-    public List<Item> getAllItemsOfRestaurant(int restaurant_id);
+    List<Menu> getMenus();
+
+    String deleteMenu(long menuId);
+
+    String updateMenu(Menu menu);
+
+    Restaurant getRestaurantById(long restaurantId);
+
+    String addNewRestaurant(Restaurant restaurant);
+
+    List<Restaurant> getRestaurants();
+
+    String deleteRestaurant(long restaurantId);
+
+    String updateRestaurant(Restaurant restaurant);
+
+    Review getReviewById(long reviewId);
+
+    List<Review> getReviewsOfItem(long itemId);
+
+    List<Review> getReviewsOfMenu(long menuId);
+
+    List<Review> getReviewsOfRestaurant(long restaurantId);
+
+    String updateReview(Review review);
+
+    String deleteReview(long reviewId);
+
+    String addReview(Review review);
+
+    String updateItem(Item item);
+
+    String deleteItem(long itemId);
+
+    String addItem(Item item);
+
+    Item getItemById(long itemId);
+
+    List<Item> getRestaurantItems(long restaurantId);
+
+    Map<Long, List<Item>> getMenuItemsByCategory(long menuId);
+
+    String deleteMenuItem(MenuItem menuItem);
+
+    String addMenuItem(MenuItem menuItem);
+
+    ItemCategory getItemCategoryById(long id);
+
+    String updateItemCategory(ItemCategory itemCategory);
+
+    String deleteItemCategory(long id);
+
+    String addItemCategory(ItemCategory itemCategory);
+
+    List<ItemCategory> getItemCategories();
+
+//    String updateItemRating(ItemRating itemRating);
+//
+//    String deleteItemRating(ItemRating itemRating);
+//
+//    String addItemRating(ItemRating itemRating);
+//
+//    List<ItemRating> getRatingsOfItem(long itemId);
+//
+//    double getItemRatingOfItem(long itemId);
+//
+//    String deleteAccountSubscription(AccountSubscription accountSubscription);
+//
+//    String addAccountSubscription(AccountSubscription accountSubscription);
 
 }
