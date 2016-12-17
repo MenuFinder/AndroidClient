cd7182ad466daca279343863503c2f5e6a5bf2e4
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2016-12-17 04:55:36 +0100

    Corrected class name spelling and the table name.

--- app/src/main/java/cat/udl/menufinder/database/SubscriptionContract.java
+++ app/src/main/java/cat/udl/menufinder/database/SubscriptionContract.java
@@ -2,9 +2,9 @@ package cat.udl.menufinder.database;
 
 import android.provider.BaseColumns;
 
-public class SuscriptionContract {
+public class SubscriptionContract {
     public static abstract class SuscriptionTable implements BaseColumns {
-        public static final String TABLE_NAME = "restaurant";
+        public static final String TABLE_NAME = "subscription";
         public static final String ACCOUNT = "account";
         public static final String RESTAURANT = "restaurant";
     }
