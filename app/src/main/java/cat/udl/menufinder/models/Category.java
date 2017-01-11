588fa0156fdc0258499a7279bb5402d0091f7abb
Marc Sánchez <sanchezfauste@gmail.com>
2017-01-11 18:34:32 +0100

    Implementing KvmSerializable on Category model

--- app/src/main/java/cat/udl/menufinder/models/Category.java
+++ app/src/main/java/cat/udl/menufinder/models/Category.java
@@ -1,8 +1,12 @@
 package cat.udl.menufinder.models;
 
-import java.io.Serializable;
+import org.ksoap2.serialization.KvmSerializable;
+import org.ksoap2.serialization.PropertyInfo;
 
-public class Category implements Serializable {
+import java.io.Serializable;
+import java.util.Hashtable;
+
+public class Category implements Serializable, KvmSerializable {
 
     private long id;
     private String name;
@@ -48,5 +52,57 @@ public class Category implements Serializable {
                 ", name='" + name + '\'' +
                 ", id=" + id +
                 '}';
+    }
+
+    @Override
+    public Object getProperty(int i) {
+        switch (i) {
+            case 0:
+                return id;
+            case 1:
+                return name;
+            case 2:
+                return description;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getPropertyCount() {
+        return 3;
+    }
+
+    @Override
+    public void setProperty(int i, Object o) {
+        switch (i) {
+            case 0:
+                id = Long.parseLong(o.toString());
+                break;
+            case 1:
+                name = o.toString();
+                break;
+            case 2:
+                description = o.toString();
+                break;
+        }
+    }
+
+    @Override
+    public void getPropertyInfo(int i, Hashtable hashtable, PropertyInfo propertyInfo) {
+        switch (i) {
+            case 0:
+                propertyInfo.type = PropertyInfo.LONG_CLASS;
+                propertyInfo.name = "id";
+                break;
+            case 1:
+                propertyInfo.type = PropertyInfo.STRING_CLASS;
+                propertyInfo.name = "name";
+                break;
+            case 2:
+                propertyInfo.type = PropertyInfo.STRING_CLASS;
+                propertyInfo.name = "description";
+                break;
+        }
     }
 }
