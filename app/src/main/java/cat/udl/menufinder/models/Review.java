c6b10f1c202b7edd1213c6ce133cafb2ba556927
Marc Sánchez <sanchezfauste@gmail.com>
2017-01-11 19:08:02 +0100

    Implementing KvmSerializable on Review model

--- app/src/main/java/cat/udl/menufinder/models/Review.java
+++ app/src/main/java/cat/udl/menufinder/models/Review.java
@@ -1,8 +1,12 @@
 package cat.udl.menufinder.models;
 
-import java.io.Serializable;
+import org.ksoap2.serialization.KvmSerializable;
+import org.ksoap2.serialization.PropertyInfo;
 
-public class Review implements Serializable {
+import java.io.Serializable;
+import java.util.Hashtable;
+
+public class Review implements Serializable, KvmSerializable {
 
     private long id;
     private String review;
@@ -69,5 +73,75 @@ public class Review implements Serializable {
                 ", parentId=" + parentId +
                 ", account='" + account + '\'' +
                 '}';
+    }
+
+    @Override
+    public Object getProperty(int i) {
+        switch (i) {
+            case 0:
+                return id;
+            case 1:
+                return review;
+            case 2:
+                return parentType;
+            case 3:
+                return parentId;
+            case 4:
+                return account;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getPropertyCount() {
+        return 5;
+    }
+
+    @Override
+    public void setProperty(int i, Object o) {
+        switch (i) {
+            case 0:
+                id = Long.parseLong(o.toString());
+                break;
+            case 1:
+                review = o.toString();
+                break;
+            case 2:
+                parentType = o.toString();
+                break;
+            case 3:
+                parentId = Long.parseLong(o.toString());
+                break;
+            case 4:
+                account = o.toString();
+                break;
+        }
+    }
+
+    @Override
+    public void getPropertyInfo(int i, Hashtable hashtable, PropertyInfo propertyInfo) {
+        switch (i) {
+            case 0:
+                propertyInfo.type = PropertyInfo.LONG_CLASS;
+                propertyInfo.name = "id";
+                break;
+            case 1:
+                propertyInfo.type = PropertyInfo.STRING_CLASS;
+                propertyInfo.name = "review";
+                break;
+            case 2:
+                propertyInfo.type = PropertyInfo.STRING_CLASS;
+                propertyInfo.name = "parentType";
+                break;
+            case 3:
+                propertyInfo.type = PropertyInfo.LONG_CLASS;
+                propertyInfo.name = "parentId";
+                break;
+            case 4:
+                propertyInfo.type = PropertyInfo.STRING_CLASS;
+                propertyInfo.name = "account";
+                break;
+        }
     }
 }
